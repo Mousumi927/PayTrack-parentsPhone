024105e94af453541e35b2a344be73d5f4579efa
Prabh Sagoo <prabhsagoo@yahoo.com>
2023-06-11 22:39:55 -0600

    Bottom bar color fixed

--- parentPhone/navigation/Tabs.jsx
+++ parentPhone/navigation/Tabs.jsx
@@ -6,21 +6,33 @@ import Accounts from '../screens/Accounts';
 import Child from '../screens/Child';
 import More from '../screens/More';
 import { auth } from "../config/firebase.js";
+import Icon from "react-native-vector-icons/FontAwesome5";
+// react-native-vector-icons/Ionicons otherwise.
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
+
+
   return (
-    <Tab.Navigator screenOptions={{headerShown:false}}>
-    <Tab.Screen name="Home" component={Home} />
-    <Tab.Screen name="Settings" component={Accounts} />
-    <Tab.Screen name="Child" component={Child} />
-    <Tab.Screen name="More" component={More} />
+
+    <Tab.Navigator screenOptions={{headerShown:false, tabBarActiveTintColor:"#4a7ae8"}}  >
+    <Tab.Screen name="Home" component={Home} options={{tabBarIcon: ({color})=> (<Icon name="home" size={25} color={color} />)}} />
+
+    <Tab.Screen name="Accounts" component={Accounts} options={{tabBarIcon: ({color})=> (<Icon name="credit-card" size={25} color={color} />)}} />
+    <Tab.Screen name="Child" component={Child} options={{tabBarIcon: ({color})=> (<Icon name="child" size={25} color={color} />)}} />
+    <Tab.Screen name="More" component={More} options={{tabBarIcon: ({color})=> (<Icon name="bars" size={25} color={color} />)}} />
   </Tab.Navigator>
   )
 }
 
 export default Tabs
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+ 
+})
